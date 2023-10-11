c1d2c281cc26ba5e176026e1f61dbf1969aa4086
Countmooshroom <benjaminwatts314@gmail.com>
2023-10-11 16:21:30 -0600

    Wrote conversion algorithm

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
-function convertToAdjMatrix(adjList) {
-    return adjList;
+function convertToAdjMatrix(lst) {
+    var matrix = [];
+    for (let i = 0; i < lst.length; i++) {
+        matrix.push([]);
+        for (let j = 0; j < lst.length; j++) {
+            if (lst[i].includes(j)) {
+                matrix[i][j] = 1;
+            }
+            else {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
 }
