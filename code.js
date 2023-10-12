fa34f500fb093f868eaf2422a9015fddde0529b8
Countmooshroom <benjaminwatts314@gmail.com>
2023-10-12 16:19:32 -0600

    Improved runtime of the algorithm

--- code.js
+++ code.js
@@ -2,9 +2,11 @@ function convertToAdjMatrix(lst) {
     var matrix = [];
     for (let i = 0; i < lst.length; i++) {
         matrix.push([]);
+        let index = 0;
         for (let j = 0; j < lst.length; j++) {
-            if (lst[i].includes(j)) {
+            if (lst[i].length > index && lst[i][index] == j) {
                 matrix[i][j] = 1;
+                index++;
             }
             else {
                 matrix[i][j] = 0;
